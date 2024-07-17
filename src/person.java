8a9238fcbab9a8ca4cc501c4b1c42bbf0c0cc3c8
yyh <2412421917@qq.com>
2024-07-17 17:24:20 +0800

    test

---
+++ src/person.java
@@ -0,0 +1,11 @@
+public class person {
+    public String name;
+    public int age;
+    public String sex;
+    public void eat(){
+        System.out.println("人吃饭");
+    }
+    public void sleep(){
+        System.out.println("人睡觉");
+    }
+}
--- src/test.java
+++ src/test.java
@@ -5,34 +5,29 @@ import java.util.Scanner;
 
 public class test {
 
-        public static void main(String[] args) {
-            List<Integer> list=new ArrayList<>();
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
 
-            for(int i=0;i<2;++i)
-            {
-                Scanner sc=new Scanner(System.in);
-                System.out.println("请随即输入数字：");
-                Integer num=sc.nextInt();
-                list.add(num);
-            }
-            Integer[] num=list.toArray(new Integer[2]);
-            int t=0;
-            for(int i=0;i<num.length-1;i++)
-            {
-                for(int j=0;j<num.length-i-1;j++)
+        for (int i = 0; i < 2; ++i) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("请随即输入数字：");
+            Integer num = sc.nextInt();
+            list.add(num);
+        }
+        Integer[] num = list.toArray(new Integer[2]);
+        int t = 0;
+        for (int i = 0; i < num.length - 1; i++) {
+            for (int j = 0; j < num.length - i - 1; j++) {
                 {
-                    if(num[j]<num[j+1])
-                    {
-                        t=num[j];
-                        num[j]=num[j+1];
-                        num[j+1]=t;
-                    }
+                    t = num[j];
+                    num[j] = num[j + 1];
+                    num[j + 1] = t;
                 }
             }
-            for(Integer in:num)
-            {
-                System.out.println(in+" ");
-            }
-
         }
+        for (Integer in : num) {
+            System.out.println(in + " ");
+        }
+
     }
+}
