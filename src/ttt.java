1ee7f78556c18a9f2d3505ec76bbed58ac8296c2
yyh <2412421917@qq.com>
2024-07-17 15:55:04 +0800

    testt

---
+++ src/ttt.java
@@ -0,0 +1,11 @@
+public class ttt {
+    public static void main(String[] args) {
+        int max=0;
+        for(int i=0;i<5;++i)
+        {
+            max=10;
+            System.out.println(max);
+        }
+        System.out.println(max);
+    }
+}
